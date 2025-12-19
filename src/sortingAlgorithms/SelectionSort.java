b61f1fbc78cb4b990febba0fdfc175b26eb6d6a4
Thanmai <bittuvsh@gmail.com>
2025-12-19 13:41:01 -0500

    Add SelectionSort code

--- src/sortingAlgorithms/SelectionSort.java
+++ src/sortingAlgorithms/SelectionSort.java
@@ -1,4 +1,34 @@
 package sortingAlgorithms;
 
+
+import java.util.Arrays;
+
 public class SelectionSort {
+    public static int getMax(int[] arr, int start, int end){
+        int max = start;
+        for(int i = start; i <= end; i++){
+            if(arr[i] > arr[max]){
+                max = i;
+            }
+        }
+        return max;
+    }
+    public static void swap(int [] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void selection(int [] arr){
+        for(int i = 0; i < arr.length; i++){
+            int lastIndex = arr.length - i - 1;
+            int maxIndex = getMax(arr, 0, lastIndex);
+            swap(arr, maxIndex, lastIndex);
+        }
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {5,4,3,2,1};
+        selection(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }

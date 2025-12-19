e7d2e825b59b3e4be3eadfb8ec003fb64b8652dc
Thanmai <bittuvsh@gmail.com>
2025-12-19 13:12:26 -0500

    Add BubbleSort

--- src/sortingAlgorithms/BubbleSort.java
+++ src/sortingAlgorithms/BubbleSort.java
@@ -1,5 +1,33 @@
 package sortingAlgorithms;
 
-public class BubbleSort {
+import java.util.Arrays;
 
+public class BubbleSort {
+    public static void swap(int [] nums, int i, int j) {
+        int temp = nums[i];
+        nums[i] = nums[j];
+        nums[j] = temp;
+    }
+    public static void bubbleSort(int[] arr) {
+        boolean swapped;
+        for (int i = 0; i < arr.length - 1; i++) {
+            swapped = false;
+            for (int j = 1; j < arr.length - i; j++) {
+                if (arr[j] < arr[j - 1]) {
+                    swap(arr, j, j - 1);
+                    swapped = true;
+                }
+            }
+            if(!swapped){
+                break;
+            }
+        }
+
+    }
+
+    public static void main(String[] args) {
+        int [] arr = {1,2,3,4,5};
+        bubbleSort(arr);
+        System.out.println(Arrays.toString(arr));
+    }
 }
